edd218d4e55dc12f53b74746503ef697ce5c3ff3
PijusSerapinasMipsas <pijus.s@mipsas.com>
2023-06-06 22:41:26 +0300

    style fixes

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,6 +2,7 @@ import React, { FC, useState } from "react";
 import Image from "next/image";
 import classNames from 'classnames';
 import emailLogoSrc from '../../../public/assets/email.svg';
+import successImgSrc from '../../../public/assets/success.svg';
 import styles from './Input.module.scss';
 
 interface InputProps {
@@ -21,20 +22,37 @@ export const Input: FC<InputProps> = ({
 }) => {
   const [value, setValue] = useState('');
 
+  const onCopy = () => {
+    navigator.clipboard.writeText('https://ratepunk.com/referral');
+  };
+
   return (
     <div>
+      {showCopy && (
+        <div className={styles.successContainer}>
+          <Image src={successImgSrc} alt="Success image" />
+          <p>Your email is confirmed!</p>
+        </div>
+      )}
       <p className={styles.failureText}>{failureText}</p>
       <div className={styles.container}>
         {!showCopy && <Image src={emailLogoSrc} alt="Email icon" />}
         <input
-          className={classNames({ [styles.marginLeft]: showCopy })}
+          className={classNames({
+            [styles.marginLeft]: showCopy,
+            [styles.disabled]: showCopy,
+          })}
           onChange={(e) => setValue(e.target.value)}
           type="email"
           value={showCopy ? 'https://ratepunk.com/referral' : value}
-          contentEditable={!showCopy}
+          contentEditable={showCopy}
           placeholder="Enter your email address"
         />
-        {showCopy && <div className={styles.copyBtn}>Copy</div>}
+        {showCopy && (
+          <div onClick={onCopy} className={styles.copyBtn}>
+            Copy
+          </div>
+        )}
       </div>
       {showButton && buttonTitle && (
         <button onClick={() => onSubmit(value)} className={styles.submitBtn}>
